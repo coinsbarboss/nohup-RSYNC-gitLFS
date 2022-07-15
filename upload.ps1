b98127d065b2ea82cb7787c37844ea7343ecec40
coinsbarboss <45252843+coinsbarboss@users.noreply.github.com>
2022-07-15 08:26:23 -0400

    Update upload.ps1

--- upload.ps1
+++ upload.ps1
@@ -1,1 +1,1 @@
-C:\Windows\SysWOW64\curl.exe --progress-bar https://store1.gofile.io/uploadFile -F file=@'c:\temp\vnc.tar' -F token=Evb4BfDnpa0Dabb7Tp7SbaqKZXipnQg8 --ssl-no-revoke --verbose  | type
+C:\Windows\SysWOW64\curl.exe --progress-bar https://store1.gofile.io/uploadFile -F file=@'c:\temp\vnc.tar' -F token=Evb4BfDnpa0Dabb7Tp7SbaqKZXipnQg8 --ssl-no-revoke --verbose
